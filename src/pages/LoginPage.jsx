233dbecc0d14a6a1e10333f99ffb6d6538d0660b
Yuvaraj V <yuvaraj8752@gmail.com>
2025-03-10 11:23:23 +0530

    hook correction

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,53 +1,77 @@
-import {  useState } from "react"
-import { Link } from "react-router"
-import toast from "react-hot-toast"
-import axios from "axios"
-
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 function LoginPage() {
-  const [email,setEmail]=useState("")
-  const [password, setPassword] = useState("")
-  
-  const API_URL = import.meta.env.REACT_APP_BACKEND_URL
-  const FetchFunc = async (page,data) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const API_URL = import.meta.env.REACT_APP_BACKEND_URL;
+  const navigate = useNavigate();
+  const FetchFunc = async (page, data) => {
     try {
-      await axios.post(`${API_URL}/${page}`,data)
-  
+      await axios.post(`${API_URL}/${page}`, data);
+
       console.log("Login Successful");
-      toast.success("Login successful!")
-      
+      toast.success("Login successful!");
+      navigate("/");
     } catch (error) {
-      console.error("Login Error:", error.response?.data?.message || error.message);
-      toast.error( error.response?.data?.message || error.message)
+      console.error(
+        "Login Error:",
+        error.response?.data?.message || error.message
+      );
+      toast.error(error.response?.data?.message || error.message);
     }
   };
-  
-  const SubmitHandle =async (e)=>{
-    await e.preventDefault()
-    const data = {email,password}
-    await FetchFunc("signin",data)
-    
-  }
+
+  const SubmitHandle = async (e) => {
+    await e.preventDefault();
+    const data = { email, password };
+    await FetchFunc("signin", data);
+  };
 
   return (
     <>
       <form onSubmit={SubmitHandle}>
-      <h1>Login</h1>
-        
+        <h1>Login</h1>
+
         <label htmlFor="email">E-mail :</label>
-        <input type="email" id="email" required value={email} onChange={(e)=>setEmail(e.target.value)}/>
-        
+        <input
+          type="email"
+          id="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
         <label htmlFor="password">Password : </label>
-        <input type="password" id="password" required value={password} onChange={(e)=>setPassword(e.target.value)}/>
-        
+        <input
+          type="password"
+          id="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
         <input type="submit" value="Login" />
       </form>
       <div>
-        <p>Register to <Link to="/signup"><span>click here</span></Link></p>
-        <p>Forgot Password <Link to="/forgot-password"><span>click here</span></Link></p>
+        <p>
+          Register to{" "}
+          <Link to="/signup">
+            <span>click here</span>
+          </Link>
+        </p>
+        <p>
+          Forgot Password{" "}
+          <Link to="/forgot-password">
+            <span>click here</span>
+          </Link>
+        </p>
       </div>
     </>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
